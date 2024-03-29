70b29d3f5fa145a34c1baa89db7bf79fe1dc810b
IhorIvantco <igorivantco@gmail.com>
2024-03-29 21:06:43 +0200

    😢 no database?

---
+++ api/main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import sqlalchemy
+
+class Product(BaseModel):
+    id: int
+    name: str
+    description: str
+    price: float
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:5173",
+    "http://localhost:5500",
+    "http://127.0.0.1",
+    "http://127.0.0.1:8080",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:5500",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def main():
+    return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+async def loadKatalog(item_id):
+    return {"item_id": item_id}
+
