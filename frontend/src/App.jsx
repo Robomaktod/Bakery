f32e0910ecbad0b72a708880b8d3238c9b37f711
IhorIvantco <igorivantco@gmail.com>
2024-04-26 17:12:24 +0300

    not working tailwind (maybe it will not work in nearest future)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,23 @@ import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 
 import { Home } from './_root/pages'
-
+import RootLayout from './_root/RootLayout'
+import AuthLayout from './_auth/AuthLayout'
 
 function App() {
   return (
     <main>
       <Routes>
-        <Route path="/login" element={ <login /> } />
-        <Route index element={ <Home /> } />
-        
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={ <login /> } />
+        </Route>
+
+        <Route element={<RootLayout />}>
+          <Route index element={ <Home /> } />
+        </Route>
+
       </Routes>
+      <p>hello</p>
     </main>
   )
 }
