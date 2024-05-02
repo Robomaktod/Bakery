4eb3727ce24f889b9c79965a7b7b7df5c18db024
Ihor <igorivantco@gmail.com>
2024-05-02 13:15:05 +0300

    Home fix

--- frontend/src/_root/pages/HomePage.jsx
+++ frontend/src/_root/pages/HomePage.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import ProductList from '/src/components/ProductList.jsx';
 
 import "../../css/Home.css"
 
 const home = () => {
   return (
-    <>
+    <div className='main-body'>
       <h1 className='main-text'>Flour power</h1>
       <h3 className='main-subtext'>Kneading <br /> Happiness Into <br /> Every Slice</h3>
-    </>
+    </div>
 
   )
 }
